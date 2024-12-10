913548943922d15ccefead764d786ff97e06ea09
asgharzady <asgharzady@gmail.com>
2024-12-10 21:42:58 +0500

    added aml mapping

--- src/main/java/com/appopay/aml/configuration/CorsConfig.java
+++ src/main/java/com/appopay/aml/configuration/CorsConfig.java
@@ -10,12 +10,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-//                .allowedOrigins("https://aml.appopay.com")
-//                .allowedOrigins("https://aml-backend.appopay.com")
-//                .allowedOrigins("https://aml-backend.chenchenapp.com")// Explicitly allow your Angular app's URL
+                .allowedOrigins(
+                        "https://aml.appopay.com",
+                        "https://aml-backend.appopay.com",
+                        "https://aml-backend.chenchenapp.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
-//                .allowCredentials(true);  // If you're sending credentials like cookies or Authorization tokens
+                .allowedHeaders("*")
+                .allowCredentials(true);  // If you're sending credentials like cookies or Authorization tokens
     }
 }
